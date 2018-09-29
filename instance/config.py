b60fa2cf92bcd17baa91ede7f597ef1490449e0c
Arthur Ngondo <ngondoarthur@gmail.com>
2018-09-29 12:57:22 +0300

    [Chore #160675022] Refactor to OOP - Database Table creation

--- instance/config.py
+++ instance/config.py
@@ -2,3 +2,24 @@
 # Enable Debugging
 
 DEBUG = True
+""" Project configuration variables """
+import os
+
+class Config():
+    """ General Config. """
+    DEBUG = False
+    SECRET = os.getenv('SECRET')
+
+class DevConfig(Config):
+    """ Development Config. """
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    """ Testing configs """
+    TESTING = True
+
+app_config = {
+    'development': DevConfig,
+    'testing': TestingConfig
+}
